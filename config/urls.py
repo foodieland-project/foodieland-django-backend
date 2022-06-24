6d57ba2a873d7baf3acc496598da670300adbaf3
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2022-06-25 02:06:01 +0430

    fix: remove re_path and add swagger.json path

--- config/urls.py
+++ config/urls.py
@@ -23,9 +23,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     # documenting api
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     path('api/recipe/', include('recipe.urls', namespace='recipe')),
     path('api/blog/', include('blog.urls', namespace='blog')),
